d8c198dcf48a27dbe27de1ad41b0440597c2d5fb
Tobias Wright <tobias.wright@gmail.com>
2023-11-18 10:41:27 -0600

    code cleanup

--- src/app/color-list/color-list.component.ts
+++ src/app/color-list/color-list.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input  } from '@angular/core';
-import { DataService } from './../services/data.service';
-import { ColorData } from '../color-data.model';
 
 @Component({
   selector: 'app-color-list',
